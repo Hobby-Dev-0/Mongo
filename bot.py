5001acdc3a80292ec84101966cb9bb05839117e0
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-09-07 14:49:33 +0530

    bot

--- bot.py
+++ bot.py
@@ -1,7 +1,36 @@
 import os
+
 comfig = os.environ.get
-import pymongo
+
+
+try:
+  
+  import motor, pymongo, dnspython
+  
+except:
+  
+  os.system("pip install pymongo && pip install dnspython && pip install motor")
+  pass
+  
 print("Welcome to pyMongo")
-client = pymongo.MongoClient(comfig("MONGO_URI", None))
-db = client['Aman']
-print(client)
+
+def env(var, default=None):
+  
+  k = os.environ.get(var, default)
+  
+  return k
+
+
+mongo = env("MONGO_URL")
+
+if not mongo:
+  
+  print("Mongo DB Not Found I am Getting Exit")
+  
+from os import system
+
+import motor.motor_asyncio
+
+database = motor.motor_asyncio.AsyncIOMotorClient(mongo)
+
+db = database ["Aman"]
